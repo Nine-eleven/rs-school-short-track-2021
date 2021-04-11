6233f701f836f0b05496b064cd20f0535e69c0bb
nine-eleven <thedevilwearspradaoops@gmail.com>
2021-04-11 13:34:15 +0300

    feat: completed task-05 and task-06

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,31 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const resultMatrix = matrix;
+  const lengthStr = matrix[0].length;
+  const lengthMatrix = matrix.length;
+
+  for (let i = 0; i < lengthMatrix; i++) {
+    for (let j = 0; j < lengthStr; j++) {
+      resultMatrix[i][j] = 0;
+      if (matrix[i][j + 1]) { resultMatrix[i][j]++; }
+      if (matrix[i + 1][j + 1]) { resultMatrix[i][j]++; }
+      if (i < lengthMatrix.length - 1 && matrix[i + 1][j]) { resultMatrix[i][j]++; }
+      if (j > 0 && i < lengthMatrix.length - 1 && matrix[i + 1][j - 1]) {
+        resultMatrix[i][j]++;
+      }
+      if (j > 0 && i >= 0 && matrix[i][j - 1]) { resultMatrix[i][j]++; }
+      if (j > 0 && i > 0 && matrix[i - 1][j - 1]) {
+        resultMatrix[i][j]++;
+      }
+      if (j >= 0 && i > 0 && matrix[i - 1][j]) { resultMatrix[i][j]++; }
+      if (j >= 0 && i > 0 && matrix[i - 1][j + 1]) {
+        resultMatrix[i][j]++;
+      }
+    }
+  }
+  return resultMatrix;
 }
 
 module.exports = minesweeper;
